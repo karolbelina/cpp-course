4e1bd7c4da4d2ef8892bc463d910a75775641d18
Karol Belina <karolbelina@gmail.com>
2018-10-09 21:10:24 +0200

    more refactoring

--- src/lab1/constants.h
+++ src/lab1/constants.h
@@ -1,16 +1,15 @@
 #pragma once
 
-const std::string DEFAULT_NAME = "untitled";	// char* const
+const std::string DEFAULT_NAME = "untitled";
 const int DEFAULT_LENGTH = 10;
 const int DEFAULT_VALUE = 0;
 
-const std::string COPY_STRING = "_copy";
 
+// --- strings ---
 const std::string TITLE_MESSAGE = "table creator v1.0";
 const std::string HELP_MESSAGE = "available commands:\n\tcreate <amount>\n\trename <table index> <name>\n\t"
 								 "resize <table index> <size>\n\tremove <table index>\n\tremove all\n\tclone <table index>\n\t"
 								 "status\n\tstatus <table index>\n\tedit <table index> <cell index> <value>\n\thelp\n\tquit";
-const std::string PROMPT_MESSAGE = "> ";
 const std::string ENTER_TABLE_NAME_MESSAGE = "enter the name of table #";
 const std::string ENTER_TABLE_LENGTH_MESSAGE = "enter the length of table #";
 const std::string COLON = ": ";
@@ -24,7 +23,7 @@ const std::string OF_TABLE_MESSAGE = "of table #";
 const std::string SET_TO_MESSAGE = " has been set to ";
 const std::string CLONED_TABLE_MESSAGE = "cloned table #";
 
-
+const std::string PROMPT_MESSAGE = "> ";
 
 // errors
 const std::string INDEX_OUT_OF_BOUNDS_MESSAGE = "index out of bounds";
@@ -60,4 +59,4 @@ const std::string NOARG_CONSTRUCTOR_MESSAGE = "\tbezp: ";
 const std::string PARAM_CONSTRUCTOR_MESSAGE = "\tparametr: ";
 const std::string COPYING_CONSTRUCTOR_MESSAGE = "\tkopiuj: ";
 const std::string DESTRUCTOR_MESSAGE = "\tusuwam: ";
-
+const std::string COPY_STRING = "_copy";
