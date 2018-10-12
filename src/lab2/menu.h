166b3166454066ff25fdb63dd7069dd5ef83924e
Karol Belina <karolbelina@gmail.com>
2018-10-12 20:32:12 +0200

    menu run method

--- src/lab2/menu.h
+++ src/lab2/menu.h
@@ -14,7 +14,31 @@ public:
 			std::cout << "  " << menuItems[i].name << " (" << menuItems[i].command << std::endl;
 		}
 
+		std::string input;
+		bool retryInput = true;
+		
+		do {
+			std::getline(std::cin, input);
 
+			if(input != "back") {
+				bool foundValidCommand = false;
+
+				for(int i = 0; i < menuItems.size(); i++) {
+					if(input == menuItems[i].command) {
+						menuItems[i].run();
+
+						foundValidCommand = true;
+					}
+				}
+
+				if(!foundValidCommand) {
+					std::cout << "invalid command" << std::endl;
+				}
+			}
+			else {
+				bool retryInput = false;
+			}
+		} while(retryInput);
 	}
 
 private:
