9ce851e55c1021395ac51b01837083aeb7a9e698
Karol Belina <karolbelina@gmail.com>
2018-10-17 22:52:42 +0200

    somethin

--- src/lab2/constants.h
+++ src/lab2/constants.h
@@ -8,7 +8,10 @@ const int PADDING = 2;
 const char OPEN_PARENTHESIS = '(';
 const char CLOSE_PARENTHESIS = ')';
 
-const std::string BACK_COMMAND = "back";
+const std::string BACK_COMMAND = "..";
+const std::string MAKE_DIRECTORY_COMMAND = "mkdir";
+const std::string MAKE_COMMAND_COMMAND = "mkcom";
+const std::string REMOVE_COMMAND = "rm";
 
 const std::string INVALID_COMMAND_MESSAGE = "invalid command";
 const std::string DEFAULT_COMMAND_MESSAGE = "default command";
