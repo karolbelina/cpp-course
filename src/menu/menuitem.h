c9b30f6a94659cc046c318ffe1096e8d2e6f2319
Karol Belina <karolbelina@gmail.com>
2018-11-01 01:59:21 +0100

    private methods

--- src/menu/menuitem.h
+++ src/menu/menuitem.h
@@ -13,8 +13,6 @@ namespace menu {
 		// MenuItem(std::string &source, size_t position, Menu* parent, Error &error);
 		virtual ~MenuItem();
 		virtual void run() = 0;
-		virtual bool search(std::string &term, std::string path, std::ostream &stream) = 0;
-		virtual bool getHelp(std::string &destination) = 0;
 		virtual std::string exportToString() = 0;
 		// static MenuItem* importFromString(std::string &source);
 		std::string getName() const;
@@ -23,6 +21,8 @@ namespace menu {
 		Menu* getRoot() const;
 
 	protected:
+		virtual bool search(std::string &term, std::string path, std::ostream &stream) = 0;
+		virtual bool getHelp(std::string &destination) = 0;
 		std::string name;
 		std::string commandString;
 		Menu* parent;
