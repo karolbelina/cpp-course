8a911f7eb1bfa81b93c2b46e118e484b51ff9039
Karol Belina <karolbelina@gmail.com>
2019-08-11 20:08:40 +0200

    Rename the remaining directory

--- src/livelab/3darray.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-bool createTable(int x, int y, int z, int**** table);
-bool changeTable(int oldX, int oldY, int oldZ, int newX, int newY, int newZ, int**** table);
-void deleteTable(int x, int y, int z, int*** table);
-void setValue(int x, int y, int z, int value, int*** table);
-double getAverage(int x, int y, int z, int*** table);
